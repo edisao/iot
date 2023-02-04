a6d9490320b1be5e0f5729bef6abeea7c3fabdbb
edisao <edison.ramirez@microtess.com>
2023-02-03 19:44:37 -0500

    Generando las pruebas unitarias

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,23 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
-
+        $response = $this->get('login');
         $response->assertStatus(200);
+    }
+
+    public function test_the_validate_data_returns_a_bad_request()
+    {
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->post('http://127.0.0.1:8000/api/v1/service/iot', ['sensor' => 'S:SENSOR001;T:19;H:42']);
+        $response->assertBadRequest();
+    }
+
+    public function test_the_save_data_returns_ok_response()
+    {
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->post('http://127.0.0.1:8000/api/v1/service/iot', ['trama' => 'S:SENSOR001;T:19;H:42']);
+        $response->assertOk();
     }
 }
